d67d9f41ec78e3bf03e6cc38c7ecbe03c5dff0b1
Krystian Maccs <krystianmaccs@gmail.com>
2022-03-17 11:09:52 +0100

    feat: add profile and signals

--- data_science/urls.py
+++ data_science/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('maccs/', admin.site.urls),
+    path('macc/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
