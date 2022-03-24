d79f3f5fc483a8dbc56f729a27319fc5e4360967
Krystian Maccs <krystianmaccs@gmail.com>
2022-03-24 13:33:31 +0100

    fix: courses model and autoscraper app deletion

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from apps.common.models import TimeStampedUUIDModel
-from apps.membership.models import Student
+from apps.membership.models import Student, Teacher
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -11,35 +11,22 @@ class Course(TimeStampedUUIDModel):
         OPTION_2 = 2, "Data Analysis"
         OPTION_3 = 3, "Data Science"
 
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    allowed_membership = models.ManyToManyField(Student)
-    course = models.IntegerField(choices=Choices.choices)
+
+    title = models.CharField(max_length=50)
+    overview = models.TextField()
+    image = models.ImageField(upload_to=None)
+    url = models.URLField()
+    tutor = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.title.title()}"
 
-class Lesson(TimeStampedUUIDModel):
-    title = models.CharField(max_length=120)
-    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
-    position = models.IntegerField()
-    video_url = models.URLField()
-    thumbnail = models.ImageField()
-
-    def __str__(self):
-        return f"{self.title.title()}"
 
 class Curriculum(TimeStampedUUIDModel):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     topic = models.CharField(max_length=100)
     sub_topic = models.CharField(max_length=150)
 
-class Course(TimeStampedUUIDModel):
-    title = models.CharField(max_length=50)
-    overview = models.TextField()
-    image = models.ImageField(upload_to=None)
-    url = models.URLField()
-    tutor = models.CharField(max_length=50)
 
 class Topic(TimeStampedUUIDModel):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -51,4 +38,7 @@ class Lesson(TimeStampedUUIDModel):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     description = models.TextField()
     objective = models.TextField()
+
+    def __str__(self):
+        return f"{self.topic.title()}"
 
