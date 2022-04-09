a8d47a39448d3448526ed8b9722600ee6e810c88
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-09 17:30:11 +0100

    fix: refactor profiles, user

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from apps.profiles.serializers import ProfileSerializer
 from rest_framework import generics
 from apps.profiles.models import Profile
+from drf_yasg.utils import swagger_auto_schema
+from django.contrib.auth import get_user_model
+from apps.user.serializers import UserSerializers
+from rest_framework.permissions import IsAuthenticated
 
 class ProfileCreateView(generics.CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = []
 
 class ProfileListView(generics.ListAPIView):
     queryset = Profile.objects.all()
